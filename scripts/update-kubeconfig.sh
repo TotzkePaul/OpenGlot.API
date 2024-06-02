bdc097b20ddd4ca953a1467437257f24b7c5589c
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 23:46:56 +0530

    Update update-kubeconfig.sh

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -6,7 +6,7 @@ rm -rf /tmp/latest_artifact_api
 latest_artifact=$(aws s3 ls s3://codepipeline-us-east-1-411495364561/openglot-api-pipelin/BuildArtif/ --recursive | sort | tail -n 1 | awk '{print $4}')
 
 # Download the latest artifact from S3
-aws s3 cp s3://codepipeline-us-east-1-411495364561/$latest_artifact /tmp/latest_artifact_web.zip
+aws s3 cp s3://codepipeline-us-east-1-411495364561/$latest_artifact /tmp/latest_artifact_api.zip
 
 # Unzip the latest artifact
 unzip /tmp/latest_artifact_api.zip -d /tmp/latest_artifact_api
