f6f4ac4a6c9ea2ac6bf23d10e63e1d6cf8d8ec7a
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 23:36:34 +0530

    Update apply-kubernetes-config.sh

--- scripts/apply-kubernetes-config.sh
+++ scripts/apply-kubernetes-config.sh
@@ -1,14 +1,9 @@
-# Remove the older artifcat
-rm -f /tmp/latest_artifact_api.zip
-rm -rf /tmp/latest_artifact_api
+if [ -f /tmp/latest_artifact_api/env_vars ]; then
+  source /tmp/latest_artifact_api/env_vars
+else
+  echo "env_vars file not found!"
+  exit 1
+fi
 
-# List all objects in the S3 bucket with the specified prefix
-latest_artifact=$(aws s3 ls s3://codepipeline-us-east-1-411495364561/openglot-api-pipelin/BuildArtif/ --recursive | sort | tail -n 1 | awk '{print $4}')
-
-# Download the latest artifact from S3
-aws s3 cp s3://codepipeline-us-east-1-411495364561/$latest_artifact /tmp/latest_artifact_api.zip
-
-# Unzip the latest artifact
-unzip /tmp/latest_artifact_api.zip -d /tmp/latest_artifact_api
-
+sed -i "s|{{IMAGE_NAME}}|$ECR_REPOSITORY_URI/$ECR_REPOSITORY_API:latest|g" "/tmp/latest_artifact_api/k8s/openglotApi-deployment.yaml"
 kubectl apply -f /tmp/latest_artifact_api/k8s/ --namespace openglot
