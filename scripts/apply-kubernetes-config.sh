2335bc82c3c165d783afb0f69b0bb26561de9e2c
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 15:55:17 +0530

    Update apply-kubernetes-config.sh

--- scripts/apply-kubernetes-config.sh
+++ scripts/apply-kubernetes-config.sh
@@ -3,7 +3,7 @@ rm -f /tmp/latest_artifact_api.zip
 rm -rf /tmp/latest_artifact_api
 
 # List all objects in the S3 bucket with the specified prefix
-latest_artifact=$(aws s3 ls s3://codepipeline-us-east-1-411495364561/openglot-web-pipelin/BuildArtif/ --recursive | sort | tail -n 1 | awk '{print $4}')
+latest_artifact=$(aws s3 ls s3://codepipeline-us-east-1-411495364561/openglot-api-pipelin/BuildArtif/ --recursive | sort | tail -n 1 | awk '{print $4}')
 
 # Download the latest artifact from S3
 aws s3 cp s3://codepipeline-us-east-1-411495364561/$latest_artifact /tmp/latest_artifact_api.zip
