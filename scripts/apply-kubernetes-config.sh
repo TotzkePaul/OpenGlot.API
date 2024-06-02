80871095ec78118a807b2010eda08899f2c3af69
devops-team-poc <70562150+devops-team-poc@users.noreply.github.com>
2024-06-02 15:42:30 +0530

    Update apply-kubernetes-config.sh

--- scripts/apply-kubernetes-config.sh
+++ scripts/apply-kubernetes-config.sh
@@ -1,2 +1,14 @@
-#!/bin/bash
-kubectl apply -f k8s/ --namespace openglot
+# Remove the older artifcat
+rm -f /tmp/latest_artifact_api.zip
+rm -rf /tmp/latest_artifact_api
+
+# List all objects in the S3 bucket with the specified prefix
+latest_artifact=$(aws s3 ls s3://codepipeline-us-east-1-411495364561/openglot-web-pipelin/BuildArtif/ --recursive | sort | tail -n 1 | awk '{print $4}')
+
+# Download the latest artifact from S3
+aws s3 cp s3://codepipeline-us-east-1-411495364561/$latest_artifact /tmp/latest_artifact_api.zip
+
+# Unzip the latest artifact
+unzip /tmp/latest_artifact_api.zip -d /tmp/latest_artifact_api
+
+kubectl apply -f /tmp/latest_artifact_api/k8s/ --namespace openglot
